c59324a39088453e76ba9bc841379159b0bc96c8
zhilothebest <aasaand@aol.com>
2016-12-30 14:43:44 -0800

    Issues 1,3, and 6 should now be resolved

--- generator_setup.sh
+++ generator_setup.sh
@@ -1,38 +1,35 @@
-#################################################################
-""" PLEASE EDIT BEFORE YOU RUN THIS IN A PERSONAL LOCATION"""
-#################################################################
+#!/bin/bash
+chmod a+x generator_setup.sh
 
-group_folder_loc="/phys/groups/tev/scratch1/users/WHHV/"
+
+#################################################################
+""" PLEASE EDIT THESE VARIABLES FOR YOUR NEEDS."""
+#################################################################
 Num_Events=10
 EVNTFile="10_events.EVNT"
 DAODFile="10_events.pool.root"
-cd $group_folder_loc
+Run_Num =304795
 
 
 ##########################
 #---Modular ROOT Setup---#
 ##########################
-T_Resources="/phys/groups/tev/scratch4/users/tommeyer"
-TMVA_SOURCE="test/setup.sh"
 
-
-
-source /phys/users/gwatts/bin/CommonScripts/configASetup.sh
-
-lsetup "root 5.34.25-x86_64-slc6-gcc48-opt"
-export PATH=/usr/bin:$PATH
-TMVA_PATH=""$T_Resources"/TMVA-v5"
-source "$TMVA_PATH"/"$TMVA_SOURCE" "$TMVA_PATH"
-echo -e "ROOT v5.34 with TMVA is now setup\n"
+#source /phys/users/gwatts/bin/CommonScripts/configASetup.sh
+#lsetup "root 5.34.25-x86_64-slc6-gcc48-opt"
+#export PATH=/usr/bin:$PATH
+#echo -e "ROOT v5.34 is now setup\n"
+#assuming youve loaded up on bash.
 
 echo "Starting the Generation Step"
-#assuming youve loaded up on bash.
+
 (
-asetup here,19.2.4.14
+#asetup here,19.2.4.14
+source $AtlasSetup/scripts/asetup.sh here,19.2.4.14
 
 #Below, edit the output file, and max events for what you need
 
-Generate_tf.py --jobConfig hss-runner.py --maxEvents $Num_Events --runNumber 304795 --outputEVNTFile $EVNTFile --ecmEnergy 13000
+Generate_tf.py --jobConfig hss-runner.py --maxEvents $Num_Events --runNumber $Run_Num --outputEVNTFile $EVNTFile --ecmEnergy 13000
 )
 
 echo "Starting the RECO step"
@@ -46,8 +43,6 @@ Reco_tf.py --inputEVNTFile $EVNTFile --outputDAODFile $DAODFile --reductionConf 
 )
 
 #python /phys/users/arbo94/Desktop/alert.py /phys/users/arbo94/Desktop/done.py
-
-
 
 
 
