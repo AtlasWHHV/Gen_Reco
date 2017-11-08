a74c9d0f783bd7b9f391bd8f243ffeb8324c7aba
Alex Schuy <alexjschuy@gmail.com>
2017-11-08 13:39:16 -0800

    Improves constants file.

--- constants.py
+++ constants.py
@@ -1,11 +1,6 @@
 servers = ['tev02', 'tev03', 'tev04', 'tev05', 'tev06', 'tev07', 'tev08', 'tev10']
 
-pids_dir = '/phys/groups/tev/scratch3/users/WHHV/.var/run'
-dispy_dir = '~/.local/lib/python2.7/site-packages/dispy'
-evnt_dir = '/phys/groups/tev/scratch3/users/WHHV/evnts'
-tmp_dir = '/phys/groups/tev/scratch3/users/WHHV/tmp'
-log_dir = '/phys/groups/tev/scratch3/users/WHHV/logs'
-aod_dir = '/phys/groups/tev/scratch3/users/WHHV/aod'
+root_dir = '/phys/groups/tev/scratch3/users/WHHV'
 reco = '/phys/users/schuya/whhv/Gen_Reco/reco.py'
 
 geometry_version = 'ATLAS-R2-2015-03-01-00_VALIDATION'
@@ -17,4 +12,12 @@ gen_release = '19.2.4.14'
 sim_release = '19.2.4.14'
 dig_release = '19.2.4.14'
 reco_release = '19.2.4.14'
+
+dispy_dir = '~/.local/lib/python2.7/site-packages/dispy'
+pids_dir = root_dir + '/.var/run'
+evnt_dir = root_dir + '/evnts'
+tmp_dir = root_dir + '/tmp'
+log_dir = root_dir + '/logs'
+aod_dir = root_dir + '/aod'
+
 
