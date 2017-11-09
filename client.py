50069d5d3df4c9eccb0e7c18c7a54fcddb95b784
Alex Schuy <alexjschuy@gmail.com>
2017-11-09 07:34:48 -0800

    Cleans up client.py, removing functionality that is no longer necessary.

--- client.py
+++ client.py
@@ -44,47 +44,39 @@ def get_job_args(batch_size, evnt_dir, log_dir, tmp_dir, aod_dir):
       job_args.append((job_version_number, job_batch_size, job_skip_events, evnt_file, log_dir, tmp_dir, aod_dir, job_id))
       job_skip_events += job_batch_size
       job_id += 1
+  with open(os.path.join(tmp_dir, 'jobs.args'), 'wb+') as fj_handle:
+       pickle.dump(job_args, fj_handle)
   return job_args
 
-def check_jobs(jobs, tmp_dir, timestamp):
+def check_jobs(jobs, job_args, tmp_dir, timestamp):
   failed_jobs = []
-  for job in jobs:
+  for job_id, job in enumerate(jobs):
+    print ('Waiting for job {:0>4}'.format(job_id))
     host = job()
     if host is None:
-      failed_jobs.append(job_args_dict[job.id])
-      print('ERROR: job {:0>4} failed!'.format(job.id))
+      failed_jobs.append(job_args[job_id])
+      print('ERROR: job {:0>4} failed!'.format(job_id))
     else:
-      print('{} executed job {:0>4} from {} to {}'.format(host, job.id, job.start_time, job.end_time))
+      print('{} executed job {:0>4} from {} to {}'.format(host, job_id, job.start_time, job.end_time))
   if len(failed_jobs) != 0:
     print('The following jobs failed ({} in total): '.format(len(failed_jobs)))
     for job in failed_jobs:
-      print('\tjob {}'.format(job[-1]))
+      print('\tjob {:0>4}'.format(job[-1]))
     with open(os.path.join(tmp_dir, 'failed_jobs.args'), 'wb+') as fj_handle:
        pickle.dump(failed_jobs, fj_handle)
     print('Run "python client.py -r {}" to redispatch failed jobs.'.format(timestamp))
 
-def dispatch_computations(job_args, test, local, tmp_dir, timestamp):
-  if not test and not local:
-    cluster = dispy.JobCluster(compute, depends=[constants])
-    http_server = dispy.httpd.DispyHTTPServer(cluster)
-    webbrowser.open('http://localhost:8181')
-    jobs = []
-  job_args_dict = {}
+def dispatch_computations(job_args, tmp_dir, timestamp):
+  cluster = dispy.JobCluster(compute, depends=[constants])
+  http_server = dispy.httpd.DispyHTTPServer(cluster)
+  webbrowser.open('http://localhost:8181')
+  jobs = []
   for job_arg in job_args:
-    job_version_number, job_batch_size, job_skip_events, evnt_file, _, _, _, job_id = job_arg
-    job_args_dict[job_id] = job_arg
-    if test:
-      print("job {:0>4}: version={}, batch_size={}, skip_events={}, evnt_file={}".format(job_id, job_version_number, job_batch_size, job_skip_events, evnt_file))
-    elif not local:
-      job = cluster.submit(*job_arg)
-      job.id = job_id
-      jobs.append(job)
-    else:
-      compute(*job_arg)
-  if not test and not local:
-    check_jobs(jobs, tmp_dir, timestamp)
-    cluster.print_status()
-    http_server.shutdown()
+    job = cluster.submit(*job_arg)
+    jobs.append(job)
+  check_jobs(jobs, job_args, tmp_dir, timestamp)
+  cluster.print_status()
+  http_server.shutdown()
 
 def clean():
   for dir in (constants.log_dir, constants.tmp_dir):
@@ -108,7 +100,6 @@ def main():
   parser.add_argument('-b', '--batch_size', type=int, default=50, help='The number of events to reco in each job.')
   parser.add_argument('--evnt_dir', default='/phys/groups/tev/scratch3/users/WHHV/evnts/tiny', help='Directory that contains evnt files.')
   parser.add_argument('-t', '--test', action='store_true', help='Attempts to find evnt files (and do cleaning, if --clean or -c is specified), but does not actually distribute jobs. Useful if you want to check that your parameters are correct without committing to actual computation.')
-  parser.add_argument('-l', '--local', action='store_true', help='Runs all computations locally, without using dispy servers. Useful for debugging computation function.')
   group = parser.add_mutually_exclusive_group()
   group.add_argument('-c', '--clean', action='store_true', help='Remove old recovery, temporary, and log files.')
   group.add_argument('-r', '--redispatch_timestamp', dest='timestamp', default=None, help='The timestamp of a run, the failed jobs of which will be redispatched.')
@@ -132,13 +123,21 @@ def main():
   if args.timestamp:
     with open(os.path.join(tmp_dir, 'failed_jobs.args'), 'rb') as fj_handle:
       failed_jobs = pickle.load(fj_handle)
-    dispatch_computations(failed_jobs, args.test, args.local, tmp_dir, timestamp)
+    dispatch_computations(failed_jobs, tmp_dir, timestamp)
   elif args.recover:
+    with open(os.path.join(tmp_dir, 'jobs.args'), 'rb') as fj_handle:
+      job_args = pickle.load(fj_handle)
     jobs = dispy.recover_jobs()
-    check_jobs(jobs, tmp_dir, timestamp)
+    print ('Recovering jobs ({} in total):'.format(len(jobs)))
+    check_jobs(jobs, job_args, tmp_dir, timestamp)
   else:
     job_args = get_job_args(args.batch_size, args.evnt_dir, log_dir, tmp_dir, aod_dir)
-    dispatch_computations(job_args, args.test, args.local, tmp_dir, timestamp)
+    if args.test:
+      for job_arg in job_args:
+        job_version_number, job_batch_size, job_skip_events, evnt_file, _, _, _, job_id = job_arg
+        print("job {:0>4}: version={}, batch_size={}, skip_events={}, evnt_file={}".format(job_id, job_version_number, job_batch_size, job_skip_events, evnt_file))
+    else:
+      dispatch_computations(job_args, tmp_dir, timestamp)
   if args.test:
     shutil.rmtree(log_dir)
     shutil.rmtree(tmp_dir)
